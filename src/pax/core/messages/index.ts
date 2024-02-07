179782f4e06438cebe2b17f3e67e6812c1e783fc
Everton Correia <evertonjcorreia@gmail.com>
2024-02-07 18:27:53 -0300

    initial commit

--- src/pax/core/messages/index.ts
+++ src/pax/core/messages/index.ts
@@ -2,4 +2,5 @@ export * from './ActualTemperatureMessage';
 export * from './BaseTemperatureMessage';
 export * from './TargetTemperatureMessage';
 export * from './HeaterSetPointMessage';
+export * from './UnknownMessage';
 export * from './MessageAbs';
