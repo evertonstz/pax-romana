471315e3af099b7ec20f63e0e486380d313ec0bf
Everton Correia <evertonjcorreia@gmail.com>
2024-02-15 17:31:09 -0300

    refactor base temp r&w abstract class

--- src/pax/core/messages/BaseTemperatureMessages.ts
+++ src/pax/core/messages/BaseTemperatureMessages.ts
@@ -24,22 +24,106 @@ export class ReadOnlyTemperatureMessage implements MessageAbs {
   }
 }
 
-export class ReadAndWriteTemperatureMessage implements ReadAndWriteMessageAbs {
+export abstract class ReadAndWriteTemperatureMessage
+  extends ReadAndWriteMessageAbs
+  implements MessageAbs
+{
   readonly temperature: number;
   readonly messageType: Messages;
   readonly packet: PaxDecryptedPacket;
 
-  constructor(messageType: Messages, packet: PaxDecryptedPacket) {
-    this.validatePacket(packet);
-    const temperature = packet.getUint16(1, true) / 10.0;
-    this.temperature = temperature;
+  constructor(
+    builder:
+      | ReadAndWriteTemperatureMessageBuilderFromPacket<ReadAndWriteTemperatureMessage>
+      | ReadAndWriteTemperatureMessageBuilderFromTemperature<ReadAndWriteTemperatureMessage>,
+    messageType: Messages,
+  ) {
+    super();
     this.messageType = messageType;
-    this.packet = packet;
+    if (builder instanceof ReadAndWriteTemperatureMessageBuilderFromPacket) {
+      // infer temperature and packet
+      this.packet = builder.getPacket();
+      this.temperature = this.packet.getUint16(1, true) / 10.0;
+    } else if (
+      builder instanceof ReadAndWriteTemperatureMessageBuilderFromTemperature
+    ) {
+      // infer packet from temperature
+      // TODO: temperature should be  temp >= 175, temp <= 215
+      this.temperature = builder.getTemperature();
+      const buffer = new ArrayBuffer(16);
+      const view = new PaxDecryptedPacket(buffer);
+      view.setUint16(1, Math.round(this.temperature * 10), true);
+      this.packet = view;
+    } else {
+      throw new Error('Invalid builder');
+    }
   }
 
-  private validatePacket(packet: PaxDecryptedPacket) {
-    if (packet.byteLength <= 1 + 2) {
-      throw new InvalidPacketTypeException('Packet too small');
+  // private validatePacket(packet: PaxDecryptedPacket) {
+  //   if (packet.byteLength <= 1 + 2) {
+  //     throw new InvalidPacketTypeException('Packet too small');
+  //   }
+  // }
+}
+
+export class ReadAndWriteTemperatureMessageBuilderFromPacket<
+  T extends ReadAndWriteTemperatureMessage,
+> {
+  private packet?: PaxDecryptedPacket;
+
+  setPacket(
+    packet: PaxDecryptedPacket,
+  ): ReadAndWriteTemperatureMessageBuilderFromPacket<T> {
+    this.packet = packet;
+    return this;
+  }
+
+  getPacket(): PaxDecryptedPacket {
+    if (!this.packet) {
+      throw new Error('Packet is not set');
     }
+    return this.packet;
+  }
+
+  build(
+    ctor: new (
+      builder: ReadAndWriteTemperatureMessageBuilderFromPacket<T>,
+    ) => T,
+  ): T {
+    if (!this.packet) {
+      throw new Error('Packet is not set');
+    }
+    return new ctor(this);
+  }
+}
+
+export class ReadAndWriteTemperatureMessageBuilderFromTemperature<
+  T extends ReadAndWriteTemperatureMessage,
+> {
+  private temperature?: number;
+
+  setTemperature(
+    temperature: number,
+  ): ReadAndWriteTemperatureMessageBuilderFromTemperature<T> {
+    this.temperature = temperature;
+    return this;
+  }
+
+  getTemperature(): number {
+    if (!this.temperature) {
+      throw new Error('Temperature is not set');
+    }
+    return this.temperature;
+  }
+
+  build(
+    ctor: new (
+      builder: ReadAndWriteTemperatureMessageBuilderFromTemperature<T>,
+    ) => T,
+  ): T {
+    if (!this.temperature) {
+      throw new Error('Temperature is not set');
+    }
+    return new ctor(this);
   }
 }
--- src/pax/core/messages/HeaterSetPointMessage.ts
+++ src/pax/core/messages/HeaterSetPointMessage.ts
@@ -1,10 +1,32 @@
+import { PaxDecryptedPacket } from '@/pax/containers/lib';
+
 import { Messages } from '../../shared/enums/Messages';
-import { PaxDecryptedPacket } from '../../shared/models/Packet';
-import { ReadAndWriteTemperatureMessage } from './BaseTemperatureMessages';
+import {
+  ReadAndWriteTemperatureMessage,
+  ReadAndWriteTemperatureMessageBuilderFromPacket,
+  ReadAndWriteTemperatureMessageBuilderFromTemperature,
+} from './BaseTemperatureMessages';
 
 export class HeaterSetPointMessage extends ReadAndWriteTemperatureMessage {
-  constructor(packet: PaxDecryptedPacket) {
-    const messageType = Messages.ATTRIBUTE_HEATER_SET_POINT;
-    super(messageType, packet);
+  constructor(
+    builder:
+      | ReadAndWriteTemperatureMessageBuilderFromPacket<HeaterSetPointMessage>
+      | ReadAndWriteTemperatureMessageBuilderFromTemperature<HeaterSetPointMessage>,
+  ) {
+    super(builder, Messages.ATTRIBUTE_HEATER_SET_POINT);
+  }
+
+  static createWithPacket(packet: PaxDecryptedPacket): HeaterSetPointMessage {
+    const builder =
+      new ReadAndWriteTemperatureMessageBuilderFromPacket<HeaterSetPointMessage>();
+    builder.setPacket(packet);
+    return new HeaterSetPointMessage(builder);
+  }
+
+  static createWithTemperature(temperature: number): HeaterSetPointMessage {
+    const builder =
+      new ReadAndWriteTemperatureMessageBuilderFromTemperature<HeaterSetPointMessage>();
+    builder.setTemperature(temperature);
+    return new HeaterSetPointMessage(builder);
   }
 }
