9abb8e92620d528b2dd5e4f29156ec84f1b8bf58
Everton Correia <evertonjcorreia@gmail.com>
2024-03-08 23:17:24 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'waikawa-gray': {
+          '50': '#f2f7fb',
+          '100': '#e7f0f8',
+          '200': '#d3e2f2',
+          '300': '#b9cfe8',
+          '400': '#9cb6dd',
+          '500': '#839dd1',
+          '600': '#6a7fc1',
+          '700': '#6374ae',
+          '800': '#4a5989',
+          '900': '#414e6e',
+          '950': '#262c40',
+      },
+      
+      }
+    },
+    
+  },
+  plugins: [],
+};
