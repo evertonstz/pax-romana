03a8443faba645af45cd656d1fb22070d1f86c40
Everton Correia <evertonjcorreia@gmail.com>
2024-03-10 22:02:29 -0300

    update device card

--- src/components/DevicesModal/DeviceCard.tsx
+++ src/components/DevicesModal/DeviceCard.tsx
@@ -1,8 +1,19 @@
 import { useDevicesLocalStorage } from '@/hooks';
 import { Pax } from '@/pax';
 import { CheckCircleTwoTone } from '@ant-design/icons';
-import { Avatar, Button, Card, Popconfirm } from 'antd';
+import { Avatar, Popconfirm } from 'antd';
 import Meta from 'antd/es/card/Meta';
+import { Check } from 'lucide-react';
+
+import { Button } from '../ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
 
 interface DeviceCardProps {
   serial: Pax.lib.PaxSerial;
@@ -12,56 +23,32 @@ const DeviceCard = ({ serial }: DeviceCardProps) => {
   const { popFromStore, saveCurrentDevice, currentDevice } =
     useDevicesLocalStorage();
 
-  const buildDeleteButton = () => {
-    return (
-      <Popconfirm
-        title={`Delete ${serial.device} ${serial.serial}?`}
-        description="Are you sure to delete this device?"
-        onConfirm={() => popFromStore(serial)}
-        okText="Yes"
-        cancelText="No"
-      >
-        <Button danger style={{ width: '80px' }}>
-          Delete
-        </Button>
-      </Popconfirm>
-    );
-  };
-
-  const buildSelectButton = () => {
-    const buttomDisabled = selectButtonDisabled();
-    return (
-      <Button
-        onClick={() => saveCurrentDevice(serial)}
-        disabled={buttomDisabled}
-        style={{ width: '80px' }}
-      >
-        {buttomDisabled ? <CheckCircleTwoTone /> : 'Select'}
-      </Button>
-    );
-  };
-
-  const selectButtonDisabled = () => {
-    return (
-      currentDevice !== undefined &&
-      currentDevice.device === serial.device &&
-      currentDevice.serial === serial.serial
-    );
-  };
+  const selectButtonDisabled =
+    currentDevice !== undefined &&
+    currentDevice.device === serial.device &&
+    currentDevice.serial === serial.serial;
 
   return (
-    <Card
-      actions={[buildSelectButton(), buildDeleteButton()]}
-      style={{ minHeight: '155px' }}
-    >
-      <Meta
-        avatar={
-          // TODO add Pax picture
-          <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />
+    <Card className="grid grid-cols-2 md:grid-cols-1">
+      <CardHeader>
+        <CardTitle>{serial.serial}</CardTitle>
+        <CardDescription className="text-xl">{serial.device}</CardDescription>
+      </CardHeader>
+      <CardFooter
+        className={
+          'grid grid-cols-1 content-between gap-2 p-6 md:grid-cols-2 md:pt-0'
         }
-        title={serial.device}
-        description={serial.serial}
-      />
+      >
+        <Button
+          disabled={selectButtonDisabled}
+          onClick={() => saveCurrentDevice(serial)}
+        >
+          {selectButtonDisabled ? <Check /> : 'Select'}
+        </Button>
+        <Button onClick={() => popFromStore(serial)} variant={'destructive'}>
+          Delete
+        </Button>
+      </CardFooter>
     </Card>
   );
 };
