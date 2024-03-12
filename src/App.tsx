c514412c21b409c9381a4a88764820590bc146ef
Everton Correia <evertonjcorreia@gmail.com>
2024-03-12 16:11:55 -0300

    finish andt removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { ConfigProvider, theme } from 'antd';
-
 import { PageLayout } from './Layout';
 import MainContent from './components/MainContent';
 import { ThemeColor } from './types';
 
-const getTheme = (color: ThemeColor) => {
-  if (color === 'light') return theme.defaultAlgorithm;
-  return theme.darkAlgorithm;
-};
-
 function App() {
   return (
-    <ConfigProvider theme={{ algorithm: getTheme('dark') }}>
-      <PageLayout>
-        <MainContent />
-      </PageLayout>
-    </ConfigProvider>
+    <PageLayout>
+      <MainContent />
+    </PageLayout>
   );
 }
 
