8bf213a76da8c63351e6e4973487e93500ed1069
Everton Correia <1169768+evertonstz@users.noreply.github.com>
2024-04-23 17:12:36 -0300

    Fix icons (#40)
    initial commit

--- src/components/MainContent/SelectedDevice/Connect.tsx
+++ src/components/MainContent/SelectedDevice/Connect.tsx
@@ -2,7 +2,7 @@ import { PaxPairing } from '@/components/Graphics';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
 import { useIsMobile } from '@/hooks';
-import { Terminal } from 'lucide-react';
+import { BluetoothIcon } from 'lucide-react';
 
 export interface IConnectProps {
   connect: () => Promise<void>;
@@ -23,7 +23,7 @@ export function Connect(props: IConnectProps) {
         Connect
       </Button>
       <Alert>
-        <Terminal className="h-4 w-4" />
+        <BluetoothIcon className="h-4 w-4" />
         <AlertTitle>Pairing mode</AlertTitle>
         <AlertDescription>
           Shake your device ultil you see the blue light.
